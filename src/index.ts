f511966ae374855aec2897b908599709eb75f183
Muneer <muneerskoppam1107@gmail.com>
2025-04-14 12:46:36 +0530

    ts config

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ dotenv.config();
 // connectDB();
 
 const app = express();
-const PORT = process.env.PORT || 3004;
+const PORT = process.env.PORT || 3000 ;
 
 // const corsOptions = {
 //     origin: process.env.FRONTEND, 
@@ -22,10 +22,10 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 
-sequelize.authenticate().then(async()=>{
-    await sequelize.sync()
-    app.listen(PORT,()=>console.log(`Server running on port ${PORT}`))
-})
+// sequelize.authenticate().then(async()=>{
+//     await sequelize.sync()
+//     app.listen(PORT,()=>console.log(`Server running on port ${PORT}`))
+// })
 
 app.use("/api", itemRoute);
 
