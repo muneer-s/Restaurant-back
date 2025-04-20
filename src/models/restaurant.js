e7459b5fa7d06624db8d8e572b72ed33610e1904
Muneer <muneerskoppam1107@gmail.com>
2025-04-20 19:49:11 +0530

    cloud DB setup

--- src/models/restaurant.js
+++ src/models/restaurant.js
@@ -1,28 +1,3 @@
-// 'use strict';
-// const {
-//   Model
-// } = require('sequelize');
-// module.exports = (sequelize, DataTypes) => {
-//   class Restaurant extends Model {
-//     /**
-//      * Helper method for defining associations.
-//      * This method is not a part of Sequelize lifecycle.
-//      * The `models/index` file will call this method automatically.
-//      */
-//     static associate(models) {
-//       // define association here
-//     }
-//   }
-//   Restaurant.init({
-//     name: DataTypes.STRING,
-//     address: DataTypes.STRING,
-//     contact: DataTypes.BIGINT
-//   }, {
-//     sequelize,
-//     modelName: 'Restaurant',
-//   });
-//   return Restaurant;
-// };
 
 
 import { Model, DataTypes } from 'sequelize';
